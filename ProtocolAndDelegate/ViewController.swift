12de89c690799e67a48220ce31a4da5eb0f0b2f3
Duyi Li <li.duy@northeastern.edu>
2022-02-23 21:33:32 -0800

    INFO6350_Assignment4

--- ProtocolAndDelegate/ViewController.swift
+++ ProtocolAndDelegate/ViewController.swift
@@ -7,13 +7,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, SendFirstAndLastNameDelegate {
+   
 
+    @IBOutlet weak var lblFirstName: UILabel!
+    
+    @IBOutlet weak var lblLastName: UILabel!
+    
+    
+    @IBOutlet weak var lblWelcome: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func getNameFromUser(_ sender: Any) {
+        performSegue(withIdentifier: "SegueGetName", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "SegueGetName" {
+            
+            let getNameVC = segue.destination as! GetNameViewController
+            
+            guard let firstName = lblFirstName.text else {return}
+            guard let lastName = lblLastName.text else{return}
+            
+            getNameVC.firstName = firstName
+            getNameVC.lastName = lastName
 
+            getNameVC.sendFirstAndLastNameDelegate = self
+        }
+    }
+        
+    func setFirstAndLastName(firstName: String, lastName: String) {
+        
+        lblFirstName.text = firstName
+        lblLastName.text = lastName
+    }
+        
+    
+    func setWelcomeText(welcomeText: String) {
+        lblWelcome.text = welcomeText
+    }
+    
 }
 
